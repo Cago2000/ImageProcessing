73d7be23d5a7e66c16dbc0a7e04edbbe6bed3d61
Cago2000 <Cago2000@gmail.com>
2025-03-25 15:40:34 +0100

    added gray scaling and resizing

--- image_operations.py
+++ image_operations.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def load_image(image_path):
     if os.path.exists(image_path):
@@ -35,3 +36,23 @@ def show_image(image):
         cv2.destroyAllWindows()
     else:
         print("Error: File not found.")
+
+def gray_scale_image(image):
+    if image is not None:
+        image = np.array(
+            [list(map(lambda bgr: int(0.299 * bgr[2] + 0.587 * bgr[1] + 0.114 * bgr[0]), row)) for row in image])
+        return np.uint8(image)
+    else:
+        return image
+
+def resize_image(image, target_width, target_height):
+    height, width = image.shape[:2]
+    x_min, x_max = 0, width - 1
+    y_min, y_max = 0, height - 1
+    reduced_image = np.zeros((target_height, target_width) + image.shape[2:], dtype=image.dtype)
+    for m in range(target_width):
+        for n in range(target_height):
+            x = x_min + (m / (target_width - 1)) * (x_max - x_min)
+            y = y_min + (n / (target_height - 1)) * (y_max - y_min)
+            reduced_image[n, m] = image[int(y), int(x)]
+    return reduced_image
