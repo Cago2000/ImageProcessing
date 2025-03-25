89ea62c394f5b1d70c71a37a9f2297bdbfc3ef2f
Cago2000 <Cago2000@gmail.com>
2025-03-25 15:40:50 +0100

    testing gray scaling and resizing

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import cv2
 import image_operations as img_ops
+from image_operations import gray_scale_image, show_image, save_image, resize_image
+
 
 def main():
-    img_path = "images/test.jpeg"  # Change this to your image path
+    img_path = "images/aquarium.jpeg"  # Change this to your image path
     img = img_ops.load_image(img_path)
 
-    if img is not None:
-        img_ops.save_image(img, "images/saved_image.jpeg")
-        img_ops.delete_image(img_path)
-        img_ops.show_image(img)
-    else:
-        img = img_ops.load_image("images/saved_image.jpeg")
-        img_ops.save_image(img, "images/test.jpeg")
+    img = gray_scale_image(img)
+    show_image(img)
+    save_image(img,"images/gray_scaled.jpeg")
 
+    h, w = img.shape[:2]
+    img = resize_image(img, int(w/4), int(h/4))
+    show_image(img)
+    save_image(img,"images/resized.jpeg")
+    
 if __name__ == "__main__":
     main()
