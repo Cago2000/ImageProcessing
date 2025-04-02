50b24a66e3a59883efb0cf8d3209b035133911b3
Cago2000 <Cago2000@gmail.com>
2025-04-02 15:05:09 +0200

    added create_image_with_gradient function

--- basic_image_operations.py
+++ basic_image_operations.py
@@ -2,12 +2,22 @@ import cv2
 import os
 import numpy as np
 
-def create_image(width, height, channels, gray_value):
+def create_image(width: int, height: int, channels: int, gray_value: int) -> np.ndarray:
     image = np.zeros([height, width, channels], dtype=np.uint8)
     image[:] = gray_value
     return image
 
-def load_image(image_path):
+def create_image_with_gradient(width: int, height: int, channels: int, brightness: int) -> np.ndarray:
+    image = np.zeros([height, width, channels], dtype=np.uint8)
+    for x in range(width):
+        for y in range(height):
+            blue = int(brightness * (x / width))
+            green = int(brightness * (y / height))
+            red = int(brightness * ((x + y) / (width + height)))
+            image[y, x] = (blue, green, red)
+    return image
+
+def load_image(image_path: str) -> np.ndarray | None:
     if os.path.exists(image_path):
         img = cv2.imread(image_path)
         if img is None:
@@ -19,21 +29,21 @@ def load_image(image_path):
         print("Error: File not found.")
         return None
 
-def save_image(image, save_path):
+def save_image(image: np.ndarray, save_path: str) -> None:
     try:
         cv2.imwrite(save_path, image)
         print(f"Image saved at {save_path}")
     except Exception as e:
         print(f"Error saving image: {e}")
 
-def delete_image(image_path):
+def delete_image(image_path: str) -> None:
     if os.path.exists(image_path):
         os.remove(image_path)
         print(f"Image deleted at {image_path}")
     else:
         print("Error: File not found.")
 
-def show_image(image, title='Image'):
+def show_image(image: np.ndarray, title: str = 'Image') -> None:
     if image is not None:
         print(f"Image displayed")
         cv2.imshow(title, image)
