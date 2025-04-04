a23249182baf286c0c381ea82bc3f584b8622609
Cago2000 <Cago2000@gmail.com>
2025-04-04 16:57:32 +0200

    implemented basic co_occurence function

---
+++ statistical_operations.py
@@ -0,0 +1,17 @@
+import numpy as np
+from typing import Callable
+
+def co_occurrence(image: np.ndarray, relation_function: Callable[[np.ndarray, int, int], bool]) -> int:
+    if len(image.shape) == 3:
+        height, width, _ = image.shape
+    else:
+        height, width = image.shape
+
+    counter = 0
+    for y in range(height):
+        for x in range(width):
+            out_of_bounds = x < 0 or y < 0 or x >= width or y >= height
+            if out_of_bounds:
+                continue
+            counter += relation_function(image, y, x)
+    return counter
