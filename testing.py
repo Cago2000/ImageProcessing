f74185dc866b2f538997fcab05ec12be7186592e
Cago2000 <Cago2000@gmail.com>
2025-04-04 16:58:11 +0200

    changed to test co_occurence

--- testing.py
+++ testing.py
@@ -1,24 +1,15 @@
+import numpy as np
 import basic_image_operations as basic_ops
-import geometrical_image_operations as geo_ops
+import statistical_operations as stat_ops
 
-img_path = "images/aquarium.jpeg"
-img = basic_ops.load_image(image_path=img_path)
+img = basic_ops.create_image_with_gradient(20, 20, 255)
+img[10, 10] = 255
+img[10, 11] = 255
+img[10, 12] = 255
+img[10, 13] = 200
+img[10, 14] = 200
 
-img = geo_ops.resize_image(image=img, target_width=600, target_height=400)
-basic_ops.show_image(image=img, title='resized image')
-
-degree = 45
-rotated_img = geo_ops.rotate_image(image=img, degree=degree)
-basic_ops.show_image(image=rotated_img, title=f"rotated image {degree} degree clockwise")
-
-degree = 59
-rotated_img = geo_ops.rotate_image(image=img, degree=degree)
-basic_ops.show_image(image=rotated_img, title=f"rotated image {degree} degree clockwise")
-
-degree = 147
-rotated_img = geo_ops.rotate_image(image=img, degree=degree)
-basic_ops.show_image(image=rotated_img, title=f"rotated image {degree} degree clockwise")
-
-degree = 360
-rotated_img = geo_ops.rotate_image(image=img, degree=degree)
-basic_ops.show_image(image=rotated_img, title=f"rotated image {degree} degree clockwise")
+relation = lambda image, y, x: np.array_equal(image[y, x], image[y, x+1]) if x+1 < image.shape[1] else False
+result = stat_ops.co_occurrence(image=img, relation_function=relation)
+print(result)
+basic_ops.show_image(img)
