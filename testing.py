cd88250ff796baee39498fa2e5e713381d2eb078
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-04-08 18:22:08 +0200

    added test example for co occurence

--- testing.py
+++ testing.py
@@ -1,4 +1,13 @@
+import numpy as np
 import basic_image_operations as basic_ops
+import statistical_operations as stat_ops
 
-img = basic_ops.load_ppm_image('images/ppm_image.ppm')
-basic_ops.show_image(img)
+test_img = np.array([[0, 0, 1, 1, 2, 3],
+                     [0, 0, 0, 1, 2, 3],
+                     [0, 0, 1, 2, 3, 3],
+                     [0, 1, 1, 2, 3, 3],
+                     [1, 2, 2, 3, 3, 3],
+                     [2, 2, 3, 3, 3, 3]])
+
+counter = stat_ops.co_occurrence(test_img, (lambda image, x, y: image[y, x] == image[y, x + 1] if x + 1 < image.shape[1] else False))
+print(counter)
