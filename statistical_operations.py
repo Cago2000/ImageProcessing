8e7ba4d41871171000f8ce907d6263c5b66c3735
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-04-10 00:36:14 +0200

    implemented mean and median functions on images

--- statistical_operations.py
+++ statistical_operations.py
@@ -10,5 +10,23 @@ def co_occurrence(image: np.ndarray, relation_function: Callable[[np.ndarray, in
     counter = 0
     for y in range(height):
         for x in range(width):
-            counter += relation_function(image, y, x)
+            counter += relation_function(image, x, y)
     return counter
+
+def median(image: np.ndarray) -> np.ndarray:
+    return image[image.shape[0] // 2][image.shape[1] // 2]
+
+def mean(image: np.ndarray) -> np.ndarray:
+    if len(image.shape) == 3:
+        height, width, _ = image.shape
+        mean_value = (0, 0, 0)
+    else:
+        height, width = image.shape
+        mean_value = 0
+    for y in range(height):
+        for x in range(width):
+            mean_value += image[y, x]
+    pixel_amount = image.shape[0]*image.shape[1]
+    print(pixel_amount)
+    mean_value = mean_value/pixel_amount
+    return mean_value
