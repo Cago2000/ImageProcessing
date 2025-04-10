79b665d780da725f5e8924eceea24e85870e9045
Cago2000 <Cago2000@gmail.com>
2025-04-10 13:21:27 +0200

    added entropy function

--- statistical_operations.py
+++ statistical_operations.py
@@ -1,7 +1,7 @@
 import math
-
 import numpy as np
 from typing import Callable
+
 
 def co_occurrence(image: np.ndarray, relation_function: Callable[[np.ndarray, int, int], bool]) -> int:
     if len(image.shape) == 3:
@@ -29,12 +29,21 @@ def variance(image: np.ndarray) -> np.float64:
 def std(image: np.ndarray) -> np.float64:
     return np.float64(math.sqrt(variance(image)))
 
+def entropy(image: np.ndarray) -> np.float64:
+    image_histogram = histogram(image)
+    entropy_value = np.float64(0.0)
+    for gray_value in range(len(image_histogram)):
+        probability_of_gray_value = image_histogram[gray_value] / np.sum(image_histogram)
+        if probability_of_gray_value > 0:
+            entropy_value += probability_of_gray_value * math.log2(probability_of_gray_value)
+    return entropy_value * -1
+
 def histogram(image: np.ndarray) -> np.ndarray | None:
     if len(image.shape) == 3:
         return None
     flattened_image = image.flatten()
     max_value = np.max(flattened_image)
-    image_histogram = np.zeros((max_value + 1))
+    image_histogram = np.zeros((max_value + 1), dtype=np.uint16)
     for pixel in flattened_image:
         image_histogram[pixel] += 1
     return image_histogram
