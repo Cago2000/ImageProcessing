611dcc780511bc64e1febd6ea04974ae8781a1a4
Cago2000 <Cago2000@gmail.com>
2025-04-14 16:26:02 +0200

    added tests for bright and dark gamma

--- testing.py
+++ testing.py
@@ -10,6 +10,15 @@ img = basic_ops.load_image(image_path='images/obama.pgm')
 img = filters.gray_scale_filter(image=img)
 basic_ops.show_image(image=img)
 
-img = stat_ops.histogram_equalization(image=img)
-basic_ops.show_image(image=img)
-basic_ops.save_image(image=img, save_path=f"images/histogram_equalized.pgm")
+histogram_equalized_img = stat_ops.histogram_equalization(image=img)
+basic_ops.show_image(image=histogram_equalized_img)
+basic_ops.save_image(image=histogram_equalized_img, save_path=f"images/histogram_equalized.pgm")
+
+brightened_img = stat_ops.gamma_equalization(image=img, gamma=0.3)
+basic_ops.show_image(image=brightened_img)
+basic_ops.save_image(image=brightened_img, save_path=f"images/brightened_gamma_equalized.pgm")
+
+darkened_img = stat_ops.gamma_equalization(image=img, gamma=3.0)
+basic_ops.show_image(image=darkened_img)
+basic_ops.save_image(image=darkened_img, save_path=f"images/darkened_gamma_equalized.pgm")
+
