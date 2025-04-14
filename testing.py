29a9b192fe41c966db31ae06859b0a9328153773
Cago2000 <Cago2000@gmail.com>
2025-04-14 17:38:32 +0200

    tested gauss filter

--- testing.py
+++ testing.py
@@ -10,15 +10,6 @@ img = basic_ops.load_image(image_path='images/obama.pgm')
 img = filters.gray_scale_filter(image=img)
 basic_ops.show_image(image=img)
 
-histogram_equalized_img = stat_ops.histogram_equalization(image=img)
-basic_ops.show_image(image=histogram_equalized_img)
-basic_ops.save_image(image=histogram_equalized_img, save_path=f"images/histogram_equalized.pgm")
 
-brightened_img = stat_ops.gamma_equalization(image=img, gamma=0.3)
-basic_ops.show_image(image=brightened_img)
-basic_ops.save_image(image=brightened_img, save_path=f"images/brightened_gamma_equalized.pgm")
-
-darkened_img = stat_ops.gamma_equalization(image=img, gamma=3.0)
-basic_ops.show_image(image=darkened_img)
-basic_ops.save_image(image=darkened_img, save_path=f"images/darkened_gamma_equalized.pgm")
-
+img = stat_ops.gauss_filter(image=img, dim=7)
+basic_ops.show_image(image=img)
