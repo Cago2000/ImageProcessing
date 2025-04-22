2caeb7d23b33ce7ea275d8ecadb4b7241efec594
Cago2000 <Cago2000@gmail.com>
2025-04-22 13:17:23 +0200

    simple start for traffic sign detection

--- testing.py
+++ testing.py
@@ -4,12 +4,22 @@ import basic_image_operations as basic_ops
 import filters
 import geometrical_image_operations as geo_ops
 import statistical_operations as stat_ops
+import traffic_sign_detection as tsd
+import cv2
+
+imgs = basic_ops.load_images(folder_path='traffic_sign_images', amount=10)
+templates = basic_ops.load_images(folder_path='traffic_sign_templates', amount=5)
+
+for i, img in enumerate(imgs):
+    imgs[i] = geo_ops.resize_image(image=img,target_width=img.shape[1]//4, target_height=img.shape[0]//4)
+
+    for j, template in enumerate(templates):
+        boxes = tsd.match_template(image=imgs[i], template=template, threshold=0.45)
+        for top_left, bottom_right in boxes:
+            cv2.rectangle(imgs[i], top_left, bottom_right, (0, 255, 0), 2)
+
+for i, img in enumerate(imgs):
+    basic_ops.show_image(image=img, title=f'Image #{i+1}')
 
 
-img = basic_ops.load_image(image_path='images/obama.pgm')
-basic_ops.show_image(image=img)
 
-img = filters.sobel_filter(image=img, mode='both')
-
-laplace_img = filters.laplace_filter(image=img, threshold=30)
-basic_ops.show_image(image=laplace_img)
