6ce9800aee2c9489098deb0ead920bfd16ff8cbc
Cago2000 <Cago2000@gmail.com>
2025-04-28 13:21:49 +0200

    changes

--- testing.py
+++ testing.py
@@ -7,20 +7,9 @@ import statistical_operations as stat_ops
 import traffic_sign_detection as tsd
 import cv2
 
-imgs = basic_ops.load_images(folder_path='traffic_sign_images', amount=10)
-templates = basic_ops.load_images(folder_path='traffic_sign_templates', amount=5)
-
-for i, img in enumerate(imgs):
-    imgs[i] = geo_ops.resize_image(image=img,target_width=img.shape[1]//4, target_height=img.shape[0]//4)
-    for template in templates:
-        for scale in [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 75, 90, 100]:
-            resized_template = geo_ops.resize_image(image=template, target_width=img.shape[1]//scale, target_height=img.shape[0]//scale)
-            boxes = tsd.match_template(image=imgs[i], template=resized_template, threshold=0.5)
-            for top_left, bottom_right in boxes:
-                cv2.rectangle(img=imgs[i], pt1=top_left, pt2=bottom_right, color=(0, 255, 0), thickness=1)
-
-for i, img in enumerate(imgs):
-    basic_ops.show_image(image=img, title=f'Image #{i+1}')
-
-
-
+img = basic_ops.load_image(image_path='traffic_sign_images/false_01.jpg')
+img = geo_ops.resize_image(image=img, target_height=img.shape[0]//8, target_width=img.shape[1]//8)
+img = filters.gray_scale_filter(image=img)
+img = filters.sobel_filter(image=img, mode='both')
+img = filters.black_white_filter(image=img, threshold=127)
+basic_ops.show_image(image=img)
