e17bb4d0979fc8de024030e98fb5b0ed47c40329
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-05-07 22:15:57 +0200

    added create_bounding_boxes and draw_bounding_box functions

--- bounding_box.py
+++ bounding_box.py
@@ -1,21 +1,22 @@
 import numpy as np
-import colors
 
 class BoundingBox:
-    def __init__(self, y: int, x: int, height: int, width: int, area: int, label: str):
+    def __init__(self, y: int, x: int, corners: list[tuple[int, int]],height: int, width: int, area: int, box_color: list[int], image_index: int):
         self.center_y = y
         self.center_x = x
+        self.box_corners = corners
         self.box_height = height
         self.box_width = width
         self.box_area = area
-        self.label = label
+        self.box_color = box_color
+        self.image_index = image_index
 
     def __repr__(self):
-        return (f"BoundingBox(center_y={self.center_y}, center_x={self.center_x}, "
-                f"height={self.box_height}, width={self.box_width}, area={self.box_area}, label='{self.label}')")
+        return (f"BoundingBox(center_y={self.center_y}, center_x={self.center_x}, box_corners={self.box_corners}, "
+                f"height={self.box_height}, width={self.box_width}, area={self.box_area}, box_color={self.box_color}, "
+                f"image_index={self.image_index})")
 
-def draw_bounding_boxes(image: np.ndarray, blobs: list, min_box_area: int, box_color: list[int]) -> tuple:
-    result = image.copy()
+def create_bounding_boxes(blobs: list, min_box_area: int, box_color: list[int], image_index: int) -> list[BoundingBox]:
     bounding_boxes = []
     for blob in blobs:
         ys, xs = zip(*blob)
@@ -29,12 +30,22 @@ def draw_bounding_boxes(image: np.ndarray, blobs: list, min_box_area: int, box_c
         if area < min_box_area:
             continue
 
-        result[top, left:right+1] = box_color
-        result[bottom, left:right+1] = box_color
+        center_y = (top + bottom) // 2
+        center_x = (left + right) // 2
+        box_corners = [top, left, bottom, right]
 
-        result[top:bottom+1, left] = box_color
-        result[top:bottom+1, right] = box_color
-
-        bounding_box_obj = BoundingBox(((top + bottom) // 2), ((right + left) // 2), height, width, area, colors.get_str_from_color(box_color))
+        bounding_box_obj = BoundingBox(center_y, center_x, box_corners, height, width, area, box_color, image_index)
         bounding_boxes.append(bounding_box_obj)
-    return result, bounding_boxes
+    return bounding_boxes
+
+def draw_bounding_box(bounding_box: BoundingBox, image: np.ndarray) -> np.ndarray:
+    top, left, bottom, right = (bounding_box.box_corners[0],
+                                bounding_box.box_corners[1],
+                                bounding_box.box_corners[2],
+                                bounding_box.box_corners[3])
+    box_color = bounding_box.box_color
+    image[top, left:right+1] = box_color
+    image[bottom, left:right+1] = box_color
+    image[top:bottom+1, left] = box_color
+    image[top:bottom+1, right] = box_color
+    return image
